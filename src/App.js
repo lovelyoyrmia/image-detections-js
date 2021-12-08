0e602e8dda940d07e79e0f140cdc25a560c729da
lovelyoyrmia <mokalulovelyo@gmail.com>
2021-12-08 19:22:04 +0700

    load models and do face detections

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef } from "react";
+import * as faceapi from "face-api.js";
+import "./App.css";
 
 function App() {
+  const imgRef = useRef();
+  const canvasRef = useRef();
+
+  const handleImage = async () => {
+    const detections = await faceapi
+      .detectAllFaces(imgRef.current, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceExpressions();
+
+    canvasRef.current.innerHtml = faceapi.createCanvasFromMedia(imgRef.current);
+    faceapi.matchDimensions(canvasRef.current, {
+      width: 940,
+      height: 650,
+    });
+    const resized = faceapi.resizeResults(detections, {
+      width: 940,
+      height: 650,
+    });
+    console.log(detections);
+    faceapi.draw.drawDetections(canvasRef.current, resized);
+  };
+
+  useEffect(() => {
+    const loadModels = () => {
+      Promise.all([
+        faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+        faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+        faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+        faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+      ])
+        .then(handleImage)
+        .catch((e) => console.log(e));
+    };
+    imgRef.current && loadModels();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img
+        crossOrigin="anonymous"
+        ref={imgRef}
+        src={
+          "https://images.pexels.com/photos/9811710/pexels-photo-9811710.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+        }
+        alt="something"
+        width="940"
+        height="650"
+      />
+      <canvas ref={canvasRef} width="940" height="650" />
     </div>
   );
 }
