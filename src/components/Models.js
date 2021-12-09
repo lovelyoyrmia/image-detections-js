09dbd62bb191347401dfc417fca035a6cbac00df
lovelyoyrmia <mokalulovelyo@gmail.com>
2021-12-09 22:44:41 +0700

    change position input and create face's found

--- src/components/Models.js
+++ src/components/Models.js
@@ -14,13 +14,24 @@ function Models({ image }) {
       imgRef.current,
       new faceapi.TinyFaceDetectorOptions()
     );
+    // canvasRef.current.innerHtml = faceapi.createCanvasFromMedia(imgRef.current);
+    // faceapi.matchDimensions(canvasRef.current, { width, height });
+
+    // const resized = faceapi.resizeResults(detections, {
+    //   width,
+    //   height,
+    // });
+
+    // faceapi.draw.drawDetections(canvasRef, resized);
+    // faceapi.draw.drawFaceExpressions(canvasRef, resized);
+    // faceapi.draw.drawContour(canvasRef, resized);
     setFaces(detections.map((d) => Object.values(d.box)));
   };
 
   const enter = () => {
     const ctx = canvasRef.current.getContext("2d");
     ctx.lineWidth = 5;
-    ctx.strokeStyle = "yellow";
+    ctx.strokeStyle = "red";
     faces.map((face) => ctx.strokeRect(...face));
   };
 
@@ -31,6 +42,7 @@ function Models({ image }) {
         faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
         faceapi.nets.faceExpressionNet.loadFromUri("/models"),
         faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+        faceapi.nets.ageGenderNet.loadFromUri("/models"),
       ])
         .then(handleImage)
         .catch((e) => console.log(e));
@@ -46,37 +58,42 @@ function Models({ image }) {
 
   return (
     <div className="container-img">
-      <div className="left-side">
-        <img crossOrigin="anonymous" ref={imgRef} src={url} alt="something" />
-        <canvas
-          onMouseEnter={enter}
-          ref={canvasRef}
-          width={width}
-          height={height}
-        />
-        {/* {faces.map((face, i) => (
-          <input
-            name={`input${i}`}
-            style={{ left: face[0], top: face[1] + face[3] + 5 }}
-            placeholder="Who's this ?"
-            key={i}
-            className="friend-input"
-            onChange={addFriend}
+      <div className="left">
+        <div className="left-side">
+          <img crossOrigin="anonymous" ref={imgRef} src={url} alt="something" />
+          <canvas
+            onMouseEnter={enter}
+            ref={canvasRef}
+            width={width}
+            height={height}
           />
-        ))} */}
+          {faces.map((face, i) => (
+            <input
+              name={`input${i}`}
+              style={{
+                left: face[0] - face[2] + face[3] - 40,
+                top: face[1] + 15,
+              }}
+              placeholder="Who's this ?"
+              key={i}
+              className="friend-input"
+              onChange={addFriend}
+            />
+          ))}
+        </div>
+        <div className="face">
+          <div className="face-found">
+            {faces.length > 0
+              ? faces.length === 1
+                ? `Found ${faces.length} face !!`
+                : `Found ${faces.length} faces !!`
+              : "Face not found"}
+          </div>
+        </div>
       </div>
       <div className="right-side">
-        <h1>Share your post</h1>
-        <input
-          type="text"
-          placeholder="What's on your mind ?"
-          className="right-input"
-        />
-        {friends && (
-          <span className="friends">
-            with <span className="name">{Object.values(friends) + " "}</span>
-          </span>
-        )}
+        <div className="right-title">Detect Faces</div>
+        {friends && <div className="name">{Object.values(friends) + " "}</div>}
         <button className="right-button">Send</button>
       </div>
     </div>
