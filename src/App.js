34a80c1adca934841cac1d8ddbc2baa564aee70f
lovelyoyrmia <mokalulovelyo@gmail.com>
2021-12-09 17:45:22 +0700

    move models to models.js

--- src/App.js
+++ src/App.js
@@ -1,56 +1,15 @@
-import { useEffect, useRef } from "react";
-import * as faceapi from "face-api.js";
+import React from "react";
 import "./App.css";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
 
 function App() {
-  const imgRef = useRef();
-  const canvasRef = useRef();
-
-  const handleImage = async () => {
-    const detections = await faceapi
-      .detectAllFaces(imgRef.current, new faceapi.TinyFaceDetectorOptions())
-      .withFaceLandmarks()
-      .withFaceExpressions();
-
-    canvasRef.current.innerHtml = faceapi.createCanvasFromMedia(imgRef.current);
-    faceapi.matchDimensions(canvasRef.current, {
-      width: 940,
-      height: 650,
-    });
-    const resized = faceapi.resizeResults(detections, {
-      width: 940,
-      height: 650,
-    });
-    console.log(detections);
-    faceapi.draw.drawDetections(canvasRef.current, resized);
-  };
-
-  useEffect(() => {
-    const loadModels = () => {
-      Promise.all([
-        faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
-        faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
-        faceapi.nets.faceExpressionNet.loadFromUri("/models"),
-        faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
-      ])
-        .then(handleImage)
-        .catch((e) => console.log(e));
-    };
-    imgRef.current && loadModels();
-  }, []);
   return (
-    <div className="App">
-      <img
-        crossOrigin="anonymous"
-        ref={imgRef}
-        src={
-          "https://images.pexels.com/photos/9811710/pexels-photo-9811710.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-        }
-        alt="something"
-        width="940"
-        height="650"
-      />
-      <canvas ref={canvasRef} width="940" height="650" />
+    <div>
+      <Nav />
+      <Body />
+      <Footer />
     </div>
   );
 }
