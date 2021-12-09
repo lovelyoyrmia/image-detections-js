958a20b1d35e7086391ece1ccb9c3c474b4bf22d
lovelyoyrmia <mokalulovelyo@gmail.com>
2021-12-09 17:47:34 +0700

    create Nav

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Nav.css";
+
+function Nav() {
+  return (
+    <div className="nav">
+      <div className="nav-container">
+        <div>Face Detection App</div>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
