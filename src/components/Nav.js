232590e30934d595dec5d37f50c730c62607b9ac
lovelyoyrmia <mokalulovelyo@gmail.com>
2021-12-09 22:45:29 +0700

    wrap nav title

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,7 @@ function Nav() {
   return (
     <div className="nav">
       <div className="nav-container">
-        <div>Face Detection App</div>
+        <div className="nav-title">Face Detection App</div>
       </div>
     </div>
   );
