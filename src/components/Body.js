c73b65b79b92f373fa51700c376550a4158dff67
lovelyoyrmia <mokalulovelyo@gmail.com>
2021-12-10 16:26:48 +0700

    create function drag n drop

--- src/components/Body.js
+++ src/components/Body.js
@@ -7,6 +7,25 @@ import DropFile from "./DropFile";
 function Body() {
   const [file, setFile] = useState();
   const [image, setImage] = useState();
+  const [highlight, setHighlight] = useState(false);
+
+  const handleHighlight = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setHighlight(true);
+  };
+  const handleUnhighlight = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setHighlight(false);
+  };
+  const handleDrop = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const file = e.dataTransfer.files[0];
+    setFile(file);
+    setHighlight(false);
+  };
 
   useEffect(() => {
     const getImage = () => {
@@ -31,30 +50,37 @@ function Body() {
       {image ? (
         <Models image={image} />
       ) : (
-        <div className="post_card">
-          <DropFile />
-          <div className="add_post">
-            <img src={imageBaby} alt="avatar" className="avatar" />
-            <div className="post_form">
-              <input
-                type="text"
-                placeholder="What's on your mind ?"
-                className="post_input"
-              />
-              <label htmlFor="file">
-                <img
-                  className="add_image"
-                  src="https://cdn-icons-png.flaticon.com/512/685/685685.png"
-                  alt="icon1"
+        <div>
+          <DropFile
+            highlight={highlight}
+            handleHighlight={handleHighlight}
+            handleUnhighlight={handleUnhighlight}
+            handleDrop={handleDrop}
+          />
+          <div className="post_card">
+            <div className="add_post">
+              <img src={imageBaby} alt="avatar" className="avatar" />
+              <div className="post_form">
+                <input
+                  type="text"
+                  placeholder="What's on your mind ?"
+                  className="post_input"
                 />
-                <button>Send</button>
-              </label>
-              <input
-                id="file"
-                onChange={(e) => setFile(e.target.files[0])}
-                type="file"
-                style={{ display: "none" }}
-              />
+                <label htmlFor="file">
+                  <img
+                    className="add_image"
+                    src="https://cdn-icons-png.flaticon.com/512/685/685685.png"
+                    alt="icon1"
+                  />
+                  <button>Send</button>
+                </label>
+                <input
+                  id="file"
+                  onChange={(e) => setFile(e.target.files[0])}
+                  type="file"
+                  style={{ display: "none" }}
+                />
+              </div>
             </div>
           </div>
         </div>
